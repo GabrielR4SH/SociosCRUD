8c3ebebf0913577b1de267d7adc9283864b6a15e
GabrielR4SH <bieelsantana23@hotmail.com>
2024-04-03 15:28:30 -0300

    First Commit + Migration Done

---
+++ database/migrations/2024_04_03_182443_create_socios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('socios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->enum('tipo', ['silver', 'gold']);
+            $table->string('cep');
+            $table->string('logradouro')->nullable();
+            $table->string('complemento')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('localidade')->nullable();
+            $table->string('uf')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('socios');
+    }
+};
